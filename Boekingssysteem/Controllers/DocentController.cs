c8ac9cf79ee5ae29c1a44b4263e7aedd73a2adbe
r0895226 <driesvanhool@gmail.com>
2023-03-12 16:13:11 +0100

    Views docenten & richtingen  beheren, landingpage, login en reset view

---
+++ Boekingssysteem/Controllers/DocentController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Boekingssysteem.Controllers
+{
+    public class DocentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
